4cd8f72619d252fae96f4895fa5a5f592f3c5b00
becquerk29 <becquerk29@gmail.com>
2023-09-01 12:35:14 -0500

    edit Signup and Login url api

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -28,7 +28,7 @@ export default function Login() {
     // auth.setIsAuthenticated(true);
     try {
       //const response = await fetch("http://localhost:3000/api/login", {
-        const response = await fetch(`${API_URL}/api/login`, {
+        const response = await fetch(`${API_URL}/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
--- src/routes/Signup.tsx
+++ src/routes/Signup.tsx
@@ -21,7 +21,7 @@ export default function Signup() {
 
     try {
       //const response = await fetch("http://localhost:3000/api/signup", {
-        const response = await fetch(`${API_URL}/api/signup`, {
+        const response = await fetch(`${API_URL}/signup`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password, name }),
